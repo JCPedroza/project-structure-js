4bf845cb6607b597ffa184d33deadcffb7b9b95c
JCPedroza <nemesis4go10@hotmail.com>
2022-09-25 11:29:03 -0500

    test: add max fibo value

--- functions/fibonacci/fibo.test.js
+++ functions/fibonacci/fibo.test.js
@@ -2,6 +2,12 @@
  * You normally need much fewer unit tests, but since the focus of this project
  * is profiling, more unit tests are added to accentuate the timing differences
  * between implementations.
+ *
+ * Number.MAX_SAFE_INTEGER is 9_007_199_254_740_991
+ * fun(78) is                 8_944_394_323_791_464
+ * fun(79) would be          14_472_334_024_676_221
+ * So the max value that can be computed using the number type is fun(78),
+ * but bigger values can be computed using the big int type.
  */
 
 const { functions, fastFunctions } = require('./fibo.repo')
@@ -36,6 +42,10 @@ for (const { fun, id } of fastFunctions) {
       expect(fun(50)).toBe(12_586_269_025)
       expect(fun(55)).toBe(139_583_862_445)
       expect(fun(60)).toBe(1_548_008_755_920)
+      expect(fun(65)).toBe(17_167_680_177_565)
+      expect(fun(70)).toBe(190_392_490_709_135)
+      expect(fun(75)).toBe(2_111_485_077_978_050)
+      expect(fun(78)).toBe(8_944_394_323_791_464) // Can't go bigger
     })
   })
 }
