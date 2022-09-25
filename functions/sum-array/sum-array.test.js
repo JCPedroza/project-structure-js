9b9bf2f946bece07f795b49bb21b52ee1418acba
JCPedroza <nemesis4go10@hotmail.com>
2022-09-25 09:05:16 -0500

    test: add float tests

--- functions/sum-array/sum-array.test.js
+++ functions/sum-array/sum-array.test.js
@@ -8,6 +8,11 @@ for (const { fun, id } of functions) {
       expect(fun([-15, 10, 5])).toBe(0)
     })
 
+    it('sums an array of floats', () => {
+      expect(fun([5.0])).toBeCloseTo(5.0, 1)
+      expect(fun([-4.9, 3.3, 9.1, -7.4, -1.6])).toBeCloseTo(-1.5, 1)
+    })
+
     it('sum of empty array is zero', () => {
       expect(fun([])).toBe(0)
     })
